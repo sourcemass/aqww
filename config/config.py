600755cf6fdaceb105ce0aaf4b0787c2f7f7f1aa
sourcemass <104501770+sourcemass@users.noreply.github.com>
2022-09-10 18:21:27 +0300

    Update config.py

--- config/config.py
+++ config/config.py
@@ -152,7 +152,7 @@ TELEGRAM_DOWNLOAD_EDIT_SLEEP = int(getenv("TELEGRAM_EDIT_SLEEP", "5"))
 
 # Your Github Repo.. Will be shown on /start Command
 
-GITHUB_REPO = getenv("GITHUB_REPO", "https://t.me/QQWGT")
+GITHUB_REPO = getenv("GITHUB_REPO", "")
 
 # Spotify Client.. Get it from https://developer.spotify.com/dashboard
 
