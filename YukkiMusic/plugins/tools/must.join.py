40bc0324cec8dfe1278c678f2101f37f7f4594d9
sourcemass <104501770+sourcemass@users.noreply.github.com>
2022-09-16 09:45:13 +0300

    Update must.join.py

--- YukkiMusic/plugins/tools/must.join.py
+++ YukkiMusic/plugins/tools/must.join.py
@@ -1,33 +1,34 @@
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from pyrogram.errors import ChatAdminRequired, UserNotParticipant, ChatWriteForbidden
-from Config import Join_CHANNEL, Channel_NAME, CHANNEL_SUDO
+from Config import Mas_CHANNEL, Mas_NAME, CHANNEL_SUDO
 from YukkiMusic import app
 
 
 @Client.on_message(~filters.edited & filters.incoming & filters.private, group=-1)
 async def must_join_channel(bot: Client, msg: Message):
-    if not YAFA_CHANNEL:  # Not compulsory
+    if not Mas_CHANNEL:  # Not compulsory
         return
     try:
         try:
             await bot.get_chat_member(CHANNEL_SUDO, msg.from_user.id)
         except UserNotParticipant:
-            if YAFA_CHANNEL.isalpha():
-                link = "https://t.me/" + YAFA_CHANNEL
+            if Mas_CHANNEL.isalpha():
+                link = "https://t.me/" + Mas_CHANNEL
             else:
                 chat_info = await bot.get_chat(CHANNEL_SUDO)
                 link = chat_info.invite_link
             try:
                 await msg.reply(
-                    f"- عذراً !! عليك الأشتراك بقناة البوت أولاً :",
+                    f"⚠️︙عذراً، عليك الانضمام الى هذهِ القناة أولاً
+⚠️︙اشترك ثم أرسل : /start",
                     disable_web_page_preview=True,
                     reply_markup=InlineKeyboardMarkup([
-                        [InlineKeyboardButton(u"{Channel_NAME}", url=link)]
+                        [InlineKeyboardButton(u"{Mas_NAME}", url=link)]
                     ])
                 )
                 await msg.stop_propagation()
             except ChatWriteForbidden:
                 pass
     except ChatAdminRequired:
-        print(f"عليك رفع البوت آدمن في القناة أولاً ؟؟ : {Join_CHANNEL} !")
+        print(f"عليك رفع البوت آدمن في القناة أولاً ؟؟ : {Mas_CHANNEL} !")
